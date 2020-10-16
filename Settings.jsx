795447af7f473f627a291e93f3cd6f850b0bfefa
skullbite <mawler12@gmail.com>
2020-10-15 20:05:42 -0400

    init

---
+++ Settings.jsx
@@ -0,0 +1,51 @@
+const { React, getModuleByDisplayName } = require("powercord/webpack")
+const { SwitchItem, TextInput, RadioGroup } = require("powercord/components/settings")
+const { AsyncComponent } = require("powercord/components")
+const FormItem = AsyncComponent.from(getModuleByDisplayName("FormItem"))
+const FormText = AsyncComponent.from(getModuleByDisplayName("FormText"))
+
+module.exports = class Settings extends React.PureComponent {
+    constructor(props) {
+        super(props)
+        this.state = { opened: { main: false } }
+    }
+    render() {
+        const { getSetting, toggleSetting, updateSetting } = this.props
+
+        return (<div>
+            <SwitchItem
+            note="If this is off, requests will be sent to e926.net with the rating:s tag in every request."
+            value={getSetting("nsfw", false)}
+            onChange={() => toggleSetting("nsfw")}>
+                NSFW 
+            </SwitchItem>
+            <TextInput
+            note="Your E621 User-Agent. This is an example of how it should look: 'MyProject/1.0 (by username on e621)'"
+            defaultValue={getSetting("headers", "Powercord/1.0 (<e621username>)")}
+            onChange={v => updateSetting("headers", v)}>
+                E621 User Agent
+            </TextInput>
+            {/* <TextInput
+            note="Put tags you don't want to see here, seperate them with a space. Don't worry about adding the - at the beginning."
+            defaultValue={getSetting("blacklistedTags", "young feral")}>
+                Blacklisted Tags
+            </TextInput>
+            <FormItem
+            title="Sorting Tags">
+                <FormText type="description">
+                Some tags you can use to sort your posts.
+                </FormText>
+            </FormItem>
+            <RadioGroup
+            options={[
+                { name: "Off", value: "off" },
+                { name: "Get posts in a random order.", value: "order:random" },
+                { name: "Get the highest scored post.", value: "order:score" }
+            ]}
+            onChange={o => {updateSetting("sortingTag", o.value)}}
+            value={getSetting("sortingTag", "off")}>
+        </RadioGroup> */}
+
+        </div>)
+    }
+}
---
+++ index.js
@@ -0,0 +1,75 @@
+const { Plugin } = require('powercord/entities')
+const { get } = require('powercord/http')
+
+module.exports = class e621 extends Plugin {
+    startPlugin() {
+        const userAgent = this.settings.get("headers", "Powercord/1.0 (<e621username>)")
+        const nsfw = this.settings.get("nsfw", false)
+        const blacklistedTags = this.settings.get("blacklistedTags", "")
+        powercord.api.settings.registerSettings("e621", {
+            category: this.entityID,
+            label: "e621",
+            render: require("./Settings.jsx"),
+        })
+        powercord.api.commands.registerCommand({
+            command: "e621",
+            aliases: ["e6"], 
+            description: "Gets images from e621",
+            usage: "{tags}",
+            async executor(args) {
+                if (!args.length) {
+                  return {
+                    send: false,
+                    result: `Invalid usage! Valid Usage: \`${this.usage.replace(
+                      "{tags}",
+                      powercord.api.commands.prefix + this.command + " <tags>"
+                    )}\``,
+                  }
+                }
+                if (userAgent === "Powercord/1.0 (<e621username>)") {
+                    return {
+                        send: false,
+                        result: "To get posts, you need to set a proper User-Agent in the settings."
+                    }
+                }
+                let p = await getPost(args)
+                if (!p.posts.length) {
+                    return {
+                        send: false,
+                        result: "No posts found. *Maybe you typed a tag in wrong?*"
+                    }
+                } 
+                const post = p.posts[0]
+                async function getPost(tags) {
+                    nsfw ? tags.push("rating:s") : {}
+                    return await get(`https://${nsfw ? "e621" : "e926"}.net/posts.json?tags=${tags.join('%20')}`)
+                    .set("User-Agent", userAgent)
+                    .then(r => {return r.body})
+                }
+                
+                /* const embed = {
+                    type: 'rich',
+                    title: `#${post.id}: ${post.tags.artist.join(", ")}`,
+                    url: `https://${nsfw ? "e621" : "e926"}.net/posts/${post.id}`,
+                    image: post.file.url,
+                    footer: {
+                        text: `Score: ${post.score.total} | Rating: ${post.rating.replace("e", "Explicit").replace("q", "Questionable").replace("s", "Safe")}`
+                    }
+                } */
+                
+                // let send = `\`\`\`diff\nRequest Debug\n- Blacklisted Tags Applied: ${blTags.length}\n- NSFW? On\n- Extra Tags: \`\`\`\n`
+                let send = `> **Post ID:** ${post.id}\n> **Rating:** ${post.rating.replace("e", "Explicit").replace("q", "Questionable").replace("s", "Safe")}\n> **Post URL:** <https://${nsfw ? "e621" : "e926"}.net/posts/${post.id}>\n> **Image:** ${post.file.url}\n> **Score:** ${post.score.total}`
+                
+                return {
+                    send: false,
+                    result: send
+                }
+            }
+        })
+    }
+    pluginWillUnload() {
+        powercord.api.commands.unregisterCommand("e621")
+        powercord.api.settings.unregisterSettings("e621")
+    }
+    
+}
