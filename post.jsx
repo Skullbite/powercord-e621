9402ec76eac98b3bc3e2fb7b5679e85b28e547d1
skullbite <mawler12@gmail.com>
2020-12-05 20:53:06 -0500

    added sending to modals

--- post.jsx
+++ post.jsx
@@ -1,11 +1,11 @@
 const { Modal } = require("powercord/components/modal")
 const { FormTitle, Card, Text, Divider, Button, FormNotice } = require("powercord/components")
 const { close } = require("powercord/modal")
-const { React, getModule } = require("powercord/webpack")
-const { clipboard } = require("electron")
+const { React, getModule, messages, channels } = require("powercord/webpack")
+const { clipboard, shell } = require("electron")
 
 const generateSources = (sources) => {
-    let sauce = sources.map(link => <a href={ link }>{ link.split("/")[2] }</a>)
+    let sauce = sources.map(link => <a href={ link } onClick={() => shell.openExternal(link)}>{ link.split("/")[2] }</a>)
     return sauce.reduce((prev, curr) => [prev, ', ', curr]) 
 }
 module.exports = ({ post, nsfw }) => <Modal size={ Modal.Sizes.LARGE }>
@@ -30,24 +30,52 @@ module.exports = ({ post, nsfw }) => <Modal size={ Modal.Sizes.LARGE }>
                     
                 </Text>
             </Card>
-            { post.file.ext.match(/(png|jpg|gif|jpeg)/) ? <img id="e6Preview" src={ post.file.url }></img> : post.file.ext === "webm" ? <video id="e6Preview" width="320" height="240" controls><source src={ post.file.url }></source></video> : <Text style={{ color: "#e45f5f" }}>Can't preview this file.</Text> }
+            { post.file.ext.match(/(png|jpg|gif|jpeg)/) ? <img id="e6Preview" src={ post.file.url }/> : post.file.ext === "webm" ? <video id="e6Preview" width="320" height="240" controls><source src={ post.file.url }></source></video> : <Text style={{ color: "#e45f5f" }}>Can't preview this file.</Text> }
         </div>
         <FormTitle tag="h4">POST TAGS</FormTitle>
         { getModule(['parse', 'parseTopic'], false).defaultRules.codeBlock
             .react({ content: `+ General (${post.tags.general.length})${post.tags.general.length ? ": " + post.tags.general.join(", ") : ""}\n+ Species (${post.tags.species.length})${post.tags.species.length ? ": " + post.tags.species.join(", ") : ""}\n+ Characters (${post.tags.character.length})${post.tags.character.length ? ": " + post.tags.character.join(", ") : ""}\n+ Meta (${post.tags.meta.length})${post.tags.meta.length ? ": " + post.tags.meta.join(", ") : ""}`, lang: "diff" }, null, {}) }
     </Modal.Content>
     <Modal.Footer>
+        
         <Button
+            style={{ marginRight: "10px"}}
             onClick={() => clipboard.writeText(post.file.url)}
-            color={Button.Colors.TRANSPARENT}
+            color={Button.Colors.BLURPLE}
         >
            Copy File URL
         </Button>
         <Button
+            style={{ marginRight: "10px"}}
             onClick={() => clipboard.writeText(`https://${nsfw ? "e621" : "e926"}.net/posts/${post.id}`)}
-            color={Button.Colors.TRANSPARENT}
+            color={Button.Colors.BLURPLE}
         >
             Copy Post URL
         </Button>
+        <Button
+            style={{ marginRight: "10px"}}
+            onClick={() => {
+                messages.sendMessage(channels.getChannelId(), { content: post.file.url })
+                close()
+            }}
+            color={post.rating === "e" || post.rating === "q" ? Button.Colors.YELLOW : Button.Colors.GREEN}
+        >
+        Send File URL
+        </Button>
+        <Button
+            style={{ marginRight: "10px"}}
+            onClick={() => {
+                messages.sendMessage(channels.getChannelId(), { content: `https://${nsfw ? "e621" : "e926"}.net/posts/${post.id}` })
+                close()
+            }}
+            color={post.rating === "e" || post.rating === "q" ? Button.Colors.YELLOW : Button.Colors.GREEN}
+        >
+        Send Post URL
+        </Button>
+        <Text
+        style={{ marginRight: "15%", marginTop: "1%" }}
+        >
+        Please be mindful of where you send posts! {">"}w0
+        </Text>
     </Modal.Footer>
 </Modal>
